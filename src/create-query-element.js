3697b77a5c6041bc9a328a5f90eb3f0116119513
Oles Odynets <olchyk98@gmail.com>
2020-11-15 14:56:28 +0100

    chore(createqueryelement): create extractAttributes function

--- src/create-query-element.js
+++ src/create-query-element.js
@@ -1,6 +1,7 @@
 const TAG_REGEX = /^[A-Za-z]+?(?=\W)/g
 const CLASSES_REGEX = /\.[A-Za-z0-9_\-|.]+/g
 const ID_REGEX = /#[A-Za-z0-9_\-|.]+/g
+const ATTRIBUTES_REGEX = /\[.+]/
 
 /**
  * Returns tag name that is used in the query,
@@ -32,4 +33,51 @@ export function extractClasses (query) {
  * */
 export function extractId (query) {
   return query.match(ID_REGEX)?.[0].slice(1) ?? null
+}
+
+/**
+ * Returns all extracted attributes from the query.
+ *
+ * @param {string} query
+ *
+ * @return {object[]}
+ * @property {string} key
+ * @property {string} value
+ * */
+export function extractAttributes (query) {
+  // Extract query chain of attributes without brackets
+  const chain = query
+    .match(ATTRIBUTES_REGEX)?.[0]
+    .slice(1, -1) ?? String()
+
+  // Splits chain into parts (["a=b","c=d"])
+  const pairs = chain
+    .split(',')
+    .map((item) => item.trim())
+    .filter(Boolean)
+
+  // Converts parts into objects with attr information
+  const attrs = pairs.map((item) => {
+    // Splits part into key and value
+    const [ key, value ] = item
+      .split('=')
+      .map((pair) => pair.trim())
+
+    // Skips this attribute if it's empty
+    // It means that user duplicated comma, like that "a=c,,b=d"
+    if (!key) {
+      // Notify user about the mistake
+      // eslint-disable-next-line no-console
+      console.warn(`Redundant comma detected in attributes in query "${query}" with attributes ${pairs}`)
+      return null
+    }
+
+    return ({
+      key,
+      value: value || 'true',
+    })
+  })
+
+  // Remove invalid attributes and return the list
+  return attrs.filter(Boolean)
 }
--- tests/create-query-element.test.js
+++ tests/create-query-element.test.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { describe, expect, it } from '@jest/globals'
-import { extractClasses, extractTagName, extractId } from '../src/create-query-element'
+import {
+  extractClasses, extractTagName, extractId, extractAttributes,
+} from '../src/create-query-element'
 
 describe('Tests for createQueryElement function', () => {
-  it('should properly recognize tag name [extractTagName]', () => {
+  it('[extractTagName] should properly recognize tag name', () => {
     const tag = 'div'
     const pay = `${tag}.span#super|background:red|`
     const extTag = extractTagName(pay)
@@ -11,45 +13,100 @@ describe('Tests for createQueryElement function', () => {
     expect(extTag).toBe(tag)
   })
 
-  it('should return null if there is no tag [extractTagName]', () => {
+  it('[extractTagName] should return null if there is no tag', () => {
     const pay = '.span#super'
     const extTag = extractTagName(pay)
 
     expect(extTag).toBe(null)
   })
 
-  it('should property recognize single classname [extractClasses]', () => {
+  it('[extractClasses] should property recognize single classname', () => {
     const pay = 'div.superclass#superid[attr]'
     const extClass = extractClasses(pay)
 
     expect(extClass).toEqual([ 'superclass' ])
   })
 
-  it('should properly recognize multiple classnames [extractClasses]', () => {
+  it('[extractClasses] should properly recognize multiple classnames', () => {
     const pay = 'div.superclass.testclass1#superid[attr]|cool:red;|'
     const extClass = extractClasses(pay)
 
     expect(extClass).toEqual([ 'superclass', 'testclass1' ])
   })
 
-  it('should return an empty array if there are no classes [extractClasses]', () => {
+  it('[extractClasses] should return an empty array if there are no classes', () => {
     const pay = 'div#superid[attr]|cool:red;|'
     const extClass = extractClasses(pay)
 
     expect(extClass).toEqual([ ])
   })
 
-  it('should properly recognize id [extractId]', () => {
+  it('[extractId] should properly recognize id', () => {
     const pay = 'div#superid[attr]|background-color:blue;|'
     const extId = extractId(pay)
 
     expect(extId).toBe('superid')
   })
 
-  it('should return null if there is no id [extractId]', () => {
+  it('[extractId] should return null if there is no id', () => {
     const pay = 'div[isup=true]'
     const extId = extractId(pay)
 
     expect(extId).toBe(null)
+  })
+
+  it('[extractAttributes] should properly recognize singular attribute', () => {
+    const pay = 'div[isup=true]'
+    const extId = extractAttributes(pay)
+
+    expect(extId).toStrictEqual([
+      {
+        key: 'isup',
+        value: 'true',
+      },
+    ])
+  })
+
+  it('[extractAttributes] should properly recognize multiple attributes', () => {
+    const pay = 'div[isup=true, isgood=true,great= false]#purple'
+    const extId = extractAttributes(pay)
+
+    expect(extId).toStrictEqual([
+      {
+        key: 'isup',
+        value: 'true',
+      },
+      {
+        key: 'isgood',
+        value: 'true',
+      },
+      {
+        key: 'great',
+        value: 'false',
+      },
+    ])
+  })
+
+  it('[extractAttributes] should set attribute value to true if attribute has only a name', () => {
+    const pay = 'div[isup=false,great]|background:blue|'
+    const extId = extractAttributes(pay)
+
+    expect(extId).toStrictEqual([
+      {
+        key: 'isup',
+        value: 'false',
+      },
+      {
+        key: 'great',
+        value: 'true',
+      },
+    ])
+  })
+
+  it('[extractAttributes] should return an empty array if no attributes were passed', () => {
+    const pay = 'div.cool|background:red;|'
+    const extId = extractAttributes(pay)
+
+    expect(extId).toStrictEqual([])
   })
 })
