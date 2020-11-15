81ac0d6efa1bb0133dfba2c14396d3d1ce523d8d
Oles Odynets <olchyk98@gmail.com>
2020-11-15 16:01:39 +0100

    chore(createqueryelement): create extractStyles function

--- src/create-query-element.js
+++ src/create-query-element.js
@@ -1,7 +1,10 @@
+import { separateByPattern } from './utils'
+
 const TAG_REGEX = /^[A-Za-z]+?(?=\W)/g
 const CLASSES_REGEX = /\.[A-Za-z0-9_\-|.]+/g
 const ID_REGEX = /#[A-Za-z0-9_\-|.]+/g
 const ATTRIBUTES_REGEX = /\[.+]/
+const STYLES_REGEX = /\|.+\|/
 
 /**
  * Returns tag name that is used in the query,
@@ -45,39 +48,29 @@ export function extractId (query) {
  * @property {string} value
  * */
 export function extractAttributes (query) {
-  // Extract query chain of attributes without brackets
-  const chain = query
-    .match(ATTRIBUTES_REGEX)?.[0]
-    .slice(1, -1) ?? String()
+  return separateByPattern(
+    query,
+    ATTRIBUTES_REGEX,
+    ',',
+    '=',
+    'true'
+  )
+}
 
-  // Splits chain into parts (["a=b","c=d"])
-  const pairs = chain
-    .split(',')
-    .map((item) => item.trim())
-    .filter(Boolean)
-
-  // Converts parts into objects with attr information
-  const attrs = pairs.map((item) => {
-    // Splits part into key and value
-    const [ key, value ] = item
-      .split('=')
-      .map((pair) => pair.trim())
-
-    // Skips this attribute if it's empty
-    // It means that user duplicated comma, like that "a=c,,b=d"
-    if (!key) {
-      // Notify user about the mistake
-      // eslint-disable-next-line no-console
-      console.warn(`Redundant comma detected in attributes in query "${query}" with attributes ${pairs}`)
-      return null
-    }
-
-    return ({
-      key,
-      value: value || 'true',
-    })
-  })
-
-  // Remove invalid attributes and return the list
-  return attrs.filter(Boolean)
+/**
+ * Returns all extracted styles from the query.
+ *
+ * @param {string} query
+ *
+ * @return {object[]}
+ * @property {string} key
+ * @property {string} value
+ * */
+export function extractStyles (query) {
+  return separateByPattern(
+    query,
+    STYLES_REGEX,
+    ';',
+    ':'
+  )
 }
